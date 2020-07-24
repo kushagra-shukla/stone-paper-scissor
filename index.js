2864c90152335558be8eba4c2e56519348f55633
Kushagra Shukla <actionative.kushagra@gmail.com>
2020-07-25 03:22:19 +0530

    Added sample image. Changed simulation count to 50.

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const players = require('./src/game_logic/init_game.js');
 const score = require('./src/data_model/scoreboard.js');
 const stonePaperScissor = require('./src/game_logic/play_game.js');
 
-const simulationCount = 5;
+const simulationCount = 50;
 
 for(let i=0;i<simulationCount;i++){
 	console.log(`\t\t\t\t\tITERATION #${i+1}:\t\t\t\t\t`);
 	stonePaperScissor.playGame(players,score);
 }
-//console.log(`${JSON.stringify(players,null,'\t')}`);
+console.log(`${JSON.stringify(players,null,'\t')}`);
